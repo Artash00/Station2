8ff583f5fec895bbc234ad5324937038331816d5
Artashes Yeritsyan <aryerits@c2r13s4.42yerevan.am>
2023-02-23 13:47:52 +0400

    Added functions

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import random
 
-cube1 = random.randrange(1, 6, 1)
-cube2 = random.randrange(1, 6, 1)
-cubeSum = cube1 + cube2
-
-print("The sum of dice is ", cube1, "+", cube2, "=", cubeSum)
+def RandomCube():
+    cube1 = random.randrange(1, 6, 1)
+    cube2 = random.randrange(1, 6, 1)
+    cubeSum = cube1 + cube2
+    print("The sum of dice is ", cube1, "+", cube2, "=", cubeSum)
+    return cubeSum
+    
+cubeSum = RandomCube()
 if cubeSum == 7 or cubeSum == 11:
     print("You won")
 elif cubeSum == 2 or cubeSum == 3 or cubeSum == 12:
@@ -14,10 +17,7 @@ elif (cubeSum >= 4 and cubeSum <= 6) or (cubeSum >= 8 and cubeSum <= 10):
     print ("Now your goal number", goalNumber)
 
     while True:
-        cube1 = random.randrange(1, 6, 1)
-        cube2 = random.randrange(1, 6, 1)
-        cubeSum = cube1 + cube2
-        print("The sum of dice is ", cube1, "+", cube2, "=", cubeSum)
+        cubeSum = RandomCube()
         if cubeSum == 7:
             print("You lose")
             break
